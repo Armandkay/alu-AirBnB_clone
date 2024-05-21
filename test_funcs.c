909542b3fdb74d7f8551d15439e5f8de3bc3c952
Kay Armand <144005933+Armandkay@users.noreply.github.com>
2024-05-21 20:08:56 +0200

    Create test_funcs.c

---
+++ test_funcs.c
@@ -0,0 +1,19 @@
+// This file is used to test all functions
+// before moving them to the main program.
+
+#include <stdio.h>
+
+// Function prototypes (declare functions here)
+void example_function();
+
+int main() {
+    // Call functions to test them
+    example_function();
+
+    return 0;
+}
+
+// Function definitions (implement functions here)
+void example_function() {
+    printf("This is an example function.\n");
+}
